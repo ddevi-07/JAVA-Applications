302de9b5a67faa53b899903fdccd837b4c67147a
ub <ub@ub-HP-EliteBook-840-G3>
2024-07-27 14:53:18 +0530

    Assignment 1

---
+++ Project1/src/developer/assignments/EB_Reading.java
@@ -0,0 +1,38 @@
+package developer.assignments;
+
+public class EB_Reading {
+	
+    //Global variables
+		int consumed_units;
+		int unit;
+		//Constructor
+		EB_Reading(int unit)
+		{
+		this.unit=unit;
+		}
+		//Main class
+		public static void main(String[]args)
+		{
+		//Create instance	
+		EB_Reading assessor =new EB_Reading(100);
+		assessor.reading();//Method calling
+		assessor.calculate();
+		}
+		//Method definition
+		long reading()
+		{
+		System.out.println("consumed_units "+consumed_units);
+		consumed_units=unit;
+		System.out.println("consumed_units "+consumed_units);
+		return consumed_units;
+
+		}
+		//Methos definition
+		void calculate()
+		{
+
+		int bill=consumed_units*7;
+		System.out.println("Paymet for the EB "+bill);
+		}
+		}
+
---
+++ Project1/src/developer/assignments/Theatre.java
@@ -0,0 +1,26 @@
+package developer.assignments;
+
+public class Theatre {
+	String movieName;
+	int movie_time;
+	
+	Theatre(String name, int time){
+		this.movieName=name;
+		this.movie_time=time;
+	}
+	public static void main(String[] args) {
+		//Create instance 
+		
+	    Theatre movie1 =new Theatre("Master", 630);
+		Theatre movie2=new Theatre("Leo",7);
+		//Method calling
+		movie1.watch_movie();
+		movie2.watch_movie();
+		
+	}
+	public void watch_movie() {
+		System.out.println("Watching "+movieName);
+		System.out.println("ShowTime is "+movie_time);
+	}
+
+}
